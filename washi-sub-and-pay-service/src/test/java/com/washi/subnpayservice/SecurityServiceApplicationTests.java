ad032458d2756ca3bf3ce84bb5f354d7050b2cbe
Fabrizio Castellano <gsht43@gmail.com>
2021-06-17 20:24:37 -0500

    test: Web Client Builder SubNPay

--- washi-sub-and-pay-service/src/test/java/com/washi/subnpayservice/SecurityServiceApplicationTests.java
+++ washi-sub-and-pay-service/src/test/java/com/washi/subnpayservice/SecurityServiceApplicationTests.java
@@ -1,13 +1,126 @@
 package com.washi.subnpayservice;
 
+import com.washi.subnpayservice.entity.Subscription;
+import com.washi.subnpayservice.entity.UserPaymentMethod;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
 
 @SpringBootTest
 class SecurityServiceApplicationTests {
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+
+
     @Test
-    void contextLoads() {
+    void paymentMethods() {
+        List<UserPaymentMethod> userPaymentMethods = webClientBuilder.build()
+                .get()
+                .uri("http://localhost:8399/subnpay/paymentmethods")
+                .retrieve()
+                .bodyToFlux(UserPaymentMethod.class)
+                .collectList()
+                .block();
+        System.out.println(userPaymentMethods);
     }
 
+    @Test
+    void newPaymentMethod() {
+        UserPaymentMethod userPaymentMethod = new UserPaymentMethod();
+        UserPaymentMethod userPaymentMethodResponse = webClientBuilder.build()
+                .post()
+                .uri("http://localhost:8399/subnpay/paymentmethods")
+                .bodyValue(userPaymentMethod)
+                .retrieve()
+                .bodyToMono(UserPaymentMethod.class)
+                .block();
+        System.out.println(userPaymentMethodResponse);
+    }
+
+    @Test
+    void userPaymentMethodById() {
+        long idL = 1;
+        String id = String.valueOf(idL);
+        UserPaymentMethod userPaymentMethod = webClientBuilder.build()
+                .get()
+                .uri("http://localhost:8399/subnpay/paymentmethods/"+id)
+                .retrieve()
+                .bodyToMono(UserPaymentMethod.class)
+                .block();
+        System.out.println(userPaymentMethod);
+    }
+
+    @Test
+    void updatePaymentMethod() {
+        long idL = 1;
+        String id = String.valueOf(idL);
+        UserPaymentMethod userPaymentMethod = new UserPaymentMethod();
+        UserPaymentMethod userPaymentMethodResponse = webClientBuilder.build()
+                .put()
+                .uri("http://localhost:8399/subnpay/paymentmethods/"+id)
+                .bodyValue(userPaymentMethod)
+                .retrieve()
+                .bodyToMono(UserPaymentMethod.class)
+                .block();
+        System.out.println(userPaymentMethodResponse);
+    }
+
+    @Test
+    void subscriptions() {
+        List<Subscription> subscriptions = webClientBuilder.build()
+                .get()
+                .uri("http://localhost:8399/subnpay/subscriptions")
+                .retrieve()
+                .bodyToFlux(Subscription.class)
+                .collectList()
+                .block();
+        System.out.println(subscriptions);
+    }
+
+    @Test
+    void newSubscription() {
+        Subscription subscription = new Subscription();
+        Subscription subscriptionResponse = webClientBuilder.build()
+                .post()
+                .uri("http://localhost:8399/subnpay/subscriptions")
+                .bodyValue(subscription)
+                .retrieve()
+                .bodyToMono(Subscription.class)
+                .block();
+        System.out.println(subscriptionResponse);
+    }
+
+
+    @Test
+    void subscriptionsById() {
+        long idL = 1;
+        String id = String.valueOf(idL);
+        Subscription subscription = webClientBuilder.build()
+                .get()
+                .uri("http://localhost:8399/subnpay/subscriptions/"+id)
+                .retrieve()
+                .bodyToMono(Subscription.class)
+                .block();
+        System.out.println(subscription);
+    }
+
+    @Test
+    void updateSubscription() {
+        long idL = 1;
+        String id = String.valueOf(idL);
+        Subscription subscription = new Subscription();
+        Subscription subscriptionResponse = webClientBuilder.build()
+                .put()
+                .uri("http://localhost:8399/subnpay/subscriptions/"+id)
+                .bodyValue(subscription)
+                .retrieve()
+                .bodyToMono(Subscription.class)
+                .block();
+        System.out.println(subscriptionResponse);
+    }
 }
