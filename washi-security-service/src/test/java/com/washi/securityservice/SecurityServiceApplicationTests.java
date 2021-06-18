71c4ce6b5dc2b5144267d1948fb7d8d4f96e75a6
Fabrizio Castellano <gsht43@gmail.com>
2021-06-17 19:43:52 -0500

    test: Web Client Builder Security

--- washi-security-service/src/test/java/com/washi/securityservice/SecurityServiceApplicationTests.java
+++ washi-security-service/src/test/java/com/washi/securityservice/SecurityServiceApplicationTests.java
@@ -1,13 +1,91 @@
 package com.washi.securityservice;
 
+import com.washi.securityservice.entity.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
 
 @SpringBootTest
 class SecurityServiceApplicationTests {
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @Test
-    void contextLoads() {
+    void users() {
+        List<User> users = webClientBuilder.build()
+                .get()
+                .uri("http://localhost:8909/security/users")
+                .retrieve()
+                .bodyToFlux(User.class)
+                .collectList()
+                .block();
+        System.out.println(users);
+    }
+
+    @Test
+    void newUser() {
+        User user = new User();
+        user.setEmail("pruebatestin3g@gmail.com");
+        user.setNumberUser("000069");
+        user.setPassword("contra");
+        user.setState("CREATED");
+        User userResponse = webClientBuilder.build()
+                .post()
+                .uri("http://localhost:8909/security/users")
+                .bodyValue(user)
+                .retrieve()
+                .bodyToMono(User.class)
+                .block();
+        System.out.println(userResponse);
+    }
+
+    @Test
+    void userById() {
+        long idL = 1;
+        String id = String.valueOf(idL);
+        User user = webClientBuilder.build()
+                .get()
+                .uri("http://localhost:8909/security/users/"+id)
+                .retrieve()
+                .bodyToMono(User.class)
+                .block();
+        System.out.println(user);
+    }
+
+    @Test
+    void updateUser() {
+        long idL = 10;
+        String id = String.valueOf(idL);
+        User user = new User();
+        user.setEmail("pruebatesting@gmail.com");
+        user.setNumberUser("000420");
+        user.setPassword("contra");
+        user.setState("CREATED");
+        User userResponse = webClientBuilder.build()
+                .put()
+                .uri("http://localhost:8909/security/users/"+id)
+                .bodyValue(user)
+                .retrieve()
+                .bodyToMono(User.class)
+                .block();
+        System.out.println(userResponse);
+    }
+
+    @Test
+    void deleteUser() {
+        long idL = 13;
+        String id = String.valueOf(idL);
+        User user = webClientBuilder.build()
+                .delete()
+                .uri("http://localhost:8909/security/users/"+id)
+                .retrieve()
+                .bodyToMono(User.class)
+                .block();
+        System.out.println(user);
     }
 
 }
